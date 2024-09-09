b0f6d0d12b6fff5e79d514c0813862d81cafd7bf
baburam1997 <shrestha.baburam1997@gmail.com>
2024-09-09 19:05:09 +0545

    adding looping list and comprehension

--- src/main/python/testing.py
+++ src/main/python/testing.py
@@ -4,8 +4,8 @@ def add(a, b):
 
 class TestMathFunctions(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(add(3, 5), 8)
+        self.assertEqual(add(3, 5), 18)
         self.assertEqual(add(-1, 1), 0)
-        self.assertEqual(add(0, 0), 0)
+        self.assertEqual(add(0, 0), 1)
 if __name__ == '__main__':
     unittest.main()
